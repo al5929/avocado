851322366137793d43621bd6221d7a09082cecd5
Beraldo Leal <bleal@redhat.com>
2022-02-10 15:18:39 -0300

    display failed tests at the end of a run
    This will introduce the new configuration option
    'job.output.testlogs.summary_statuses' that will improve the readability
    when running multiple tests inside a CI job without the need to run a
    second command or grep to find failed tests. This is related to: #5200
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- avocado/plugins/testlogs.py
+++ avocado/plugins/testlogs.py
@@ -55,9 +55,6 @@ class TestLogsUI(JobPre, JobPost):
         if job.config.get('stdout_claimed_by') is not None:
             return
 
-        if job.config.get('stdout_claimed_by') is not None:
-            return
-
         try:
             with open(os.path.join(job.logdir, 'results.json'),
                       encoding='utf-8') as json_file:
